e01171ed582290ad0a6b40503cddde541c081460
benjamin manda ilunga <benjaminilunga011@gmail.com>
2024-03-16 14:26:04 -0400

    gateway select by id

--- back/IdentityService/ApiGateway/obj/Debug/net8.0/ApiGateway.AssemblyInfo.cs
+++ back/IdentityService/ApiGateway/obj/Debug/net8.0/ApiGateway.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2025e7267e988dd15c42e49abd83f6c4e61c4cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95144a6a84728f3b8d46e7700c30f1d79ebd7429")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
